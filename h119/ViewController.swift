7c83eb4461ba23b0484e17c97e8756cffe2e028d
Kazimir Makaranka <m2014ky@gmail.com>
2023-02-20 15:15:08 +0300

    Initial Commit

---
+++ h119/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  h119
+//
+//  Created by Kazimir on 20.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
